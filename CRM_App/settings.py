5c75a58d51fbbb5bc8bf97f3983a6a83744ecc08
venkat <venkatkarthi770@gmail.com>
2024-08-19 22:05:13 +0530

    new changes

--- CRM_App/settings.py
+++ CRM_App/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-qrsnt45lo%+d0^m$ld41pauqk=irkz()5wc!hoy273=rhkv)!&
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # Application definition
