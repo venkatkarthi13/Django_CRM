8af4d60fe264c8d8ae31526bf3d7016fc131cef1
venkat <venkatkarthi770@gmail.com>
2024-08-20 07:56:53 +0530

    new changes

--- CRM_App/settings.py
+++ CRM_App/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qrsnt45lo%+d0^m$ld41pauqk=irkz()5wc!hoy273=rhkv)!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 # Application definition
 INSTALLED_APPS = [
