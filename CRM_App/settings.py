67f903507c51e1a26524ebe56d4e453c0f8383c5
venkat <venkatkarthi770@gmail.com>
2024-08-20 16:39:23 +0530

    new changes

--- CRM_App/settings.py
+++ CRM_App/settings.py
@@ -84,8 +84,9 @@ DATABASES = {
 }
 
 # Override with DATABASE_URL if available (usually for production)
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(default=os.getenv('venkat:venkat98@localhost:5432/CRM_Apk'))
+
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
